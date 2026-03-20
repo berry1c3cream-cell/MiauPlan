8177cbc3d84132347a665526009b5d135f682b26
berry1c3cream-cell <berry1c3cream@gmail.com>
2026-03-19 20:31:14 -0600

    Se hizo funcional el boton de agregar gatos

--- app/src/main/java/com/example/miauplan1ver/CrearGatoActivity.kt
+++ app/src/main/java/com/example/miauplan1ver/CrearGatoActivity.kt
@@ -5,25 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class CrearGatoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_cat)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val boton = findViewById<FloatingActionButton>(R.id.addCat)
-
-        boton.setOnClickListener {
-            val intent = Intent(this, CrearGatoActivity::class.java)
-            startActivity(intent)
         }
     }
 }
