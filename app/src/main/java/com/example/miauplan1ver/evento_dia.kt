c5301b2cdf720115837ba2f87e45c1b1bc77dd93
berry1c3cream-cell <berry1c3cream@gmail.com>
2026-03-26 13:23:53 -0600

    Se agrego una conexión en todas las pantallas

--- app/src/main/java/com/example/miauplan1ver/evento_dia.kt
+++ app/src/main/java/com/example/miauplan1ver/evento_dia.kt
@@ -7,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CrearGatoActivity : AppCompatActivity() {
+class EventoDiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.add_cat)
+        setContentView(R.layout.evento_dia)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
